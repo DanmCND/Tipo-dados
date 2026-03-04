b1b0ecfce6345b094ffb5575c1cb18d1708d38f7
Danm <digital.danmcnd@gmail.com>
2026-03-04 18:21:22 -0300

    Add files via upload

---
+++ exercicio_bonus.py
@@ -0,0 +1,30 @@
+import os
+def clear():
+    os.system('cls')
+
+
+clear()
+
+lista_produtos =[
+    {'nome': 'nsx', 'preco': 15000000},
+    {'nome': 'rx7', 'preco': 2000000},
+    {'nome': 'supra', 'preco': 1200000}
+]
+
+produto = {
+    'nome':input('nome do produto:'),
+    'preco':float(input('Valor do produto:')),
+    'quantidade':int(input('Quantidade em estoque:')),
+    'categoria':input('Categoria do seu produto:')
+}
+
+lista_produtos.append({'nome': produto['nome'], 'preco': produto['preco']})
+
+for lista in lista_produtos:
+    print(lista)
+
+
+
+'''print(lista_produtos)
+print(f'Produto:{produto['nome']}')
+print(f'Valor: R${produto['preço']}')'''
